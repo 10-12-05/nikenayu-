80b64759c3f72ad8d73535b526787fdd19f1e06b
NIKEN AYU PRAMESWARI <nikenayuprameswari@gmail.com>
2026-05-26 14:35:35 +0700

    update

---
+++ h_produk.php
@@ -0,0 +1,26 @@
+<?php
+include "koneksi.php";
+
+$id = $_GET['id'];
+
+// ambil nama file gambar
+$get = mysqli_query($conn, "SELECT gambar FROM products WHERE id='$id'");
+$data = mysqli_fetch_array($get);
+$gambar = $data['gambar'];
+$target_gambar = __DIR__ . "/produk_img/" . $gambar;
+
+if ($gambar != "" && file_exists($target_gambar)) {
+    unlink($target_gambar);
+}
+// ------------------------------------
+
+//hapus data dari database
+$hapus = mysqli_query($conn, "DELETE FROM products WHERE id='$id'");
+
+if ($hapus) {
+    echo "<script>alert('Data Berhasil Dihapus')</script>";
+    header("refresh:0, Data_produk.php");
+} else {
+    echo "<script>alert('Data Gagal Dihapus')</script>";
+    header("refresh:0, Data_produk.php");
+}
